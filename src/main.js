21260c032453b4435433d7811b2ceea0c29c9e81
yuuho <y.horiuchi@suou.waseda.jp>
2022-01-22 15:37:49 +0900

    公開に向けて修正

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const main = ()=>{
                     'gameResolution'   : "HD (1280x720)",      // オフスクリーンセーフティゾーンの画面サイズ
                     'textureResolution': "2048",            // テクスチャのサイズ
                     'displayResolution': "Middle",         // 実際に映る画面の画素数上限
-                    'debugBoot'        : true         };  // デバッグモードのまま起動するか否か
+                    'debugBoot'        : false         };  // デバッグモードのまま起動するか否か
     const game = new Game( config ); // ゲームの作成
     game.start(); // ゲームの開始
 };
